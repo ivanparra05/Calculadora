8be2425e3d277f9fa9e910faaae08c90c50f732a
Ivan Parra <iparmel1008@g.educaand.es>
2025-10-30 12:11:48 +0100

    Version 4: Agregar Division

--- Calculadora/src/Calculadora/Calculadora.java
+++ Calculadora/src/Calculadora/Calculadora.java
@@ -11,4 +11,10 @@ public class Calculadora {
 	 public double multiplicar(double a, double b) {
 		 return a * b;
 		}
+	 public double dividir(double a, double b) {
+		 if (b == 0) {
+		 throw new IllegalArgumentException("No se puede dividir entre cero");
+		 }
+		 return a / b;
+		}
 }
